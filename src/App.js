7a68b564882ad94f2ee14dd6447f8896a5f4ff4a
Carlos Santoss <carlos_santos0702@hotmail.com>
2021-06-22 09:24:04 -0300

    Editando estilos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import { Search } from "./components/Search";
+import { SearchAnime } from "./components/SearchAnime";
 
 import './styles.css'
 
@@ -8,28 +8,28 @@ export default function App() {
   const [text, setText] = useState('')
   
   useEffect(() => {
-    if(text) {
+    //if(text) {
       fetch(`https://kitsu.io/api/edge/anime?filter[text]=${text}`)
         .then((response) => response.json())
         .then((response) => {
           setInfo(response)
         })
-      }
+     // }
   }, [text]);
 
   return (
     <div className="App">
       <h1>Buscar Animes</h1>
-        <Search 
+        <SearchAnime 
           value={text} 
           onChange={(search)=> setText(search)} 
         />
 
         {info.data && (
-          <ul className="animes-list" >
+          <ul className="animes-list">
             {info.data.map((anime) => (
               <li key={anime.id}>
-                  <img src={anime.attributes.posterImage.small} />
+                  <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle} />
                   {anime.attributes.canonicalTitle}
               </li>
             ))}
--- src/components/SearchAnime.js
+++ src/components/SearchAnime.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export function Search({ value, onChange }) {
+export function SearchAnime({ value, onChange }) {
     function handleChange(event) {
         onChange(event.target.value);
     }
 
     return(
-        <input 
+        <input
+            className="search-input"
             type="search" 
             value={value} 
             onChange={handleChange} 
