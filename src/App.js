9065881d7734c270067bf257dfcacfb24796e7fd
Carlos Santoss <carlos_santos0702@hotmail.com>
2021-06-21 21:43:19 -0300

    Projeto iniciado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import { Search } from "./components/Search";
 
-function App() {
+import './styles.css'
+
+export default function App() {
+  const [text, setText] = useState('')
+  console.log(text)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Buscar Animes</h1>
+        <Search 
+          value={text} 
+          onchange={(search)=> setText(search)} 
+        />
+    </div>   
   );
 }
 
-export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export function Search({ value, onChange }) {
+    function handleChange(event) {
+        onChange(event.target.value)
+    }
+
+    return(
+        <input 
+            type="search" 
+            value={value} 
+            onChange={handleChange} 
+        />
+    )
+}
